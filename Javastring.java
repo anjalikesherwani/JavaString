4575a3fdc7c073225591f18bf2b090a98c4631dd
anjalikesherwani <122610979+anjalikesherwani@users.noreply.github.com>
2024-06-02 13:08:02 +0530

    String

--- Javastring.java
+++ Javastring.java
@@ -3,5 +3,14 @@ public class Javastring {
         String greeting = "Hello";
         System.out.println(greeting);
 
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("The length of the txt string is: " + txt.length());
+
+        String text = "Hello World";
+        System.out.println(text.toUpperCase());   // Outputs "HELLO WORLD"
+        System.out.println(text.toLowerCase());   // Outputs "hello world"
+    
+        String Txt = "Please locate where 'locate' occurs!";
+        System.out.println(Txt.indexOf("locate")); // Outputs 7
     }
 }
